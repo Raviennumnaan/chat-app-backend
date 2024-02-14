00ef565cdd0f1dcebc23171685982f6fc3e26cf3
Ravishankar M <raviennumnaan@gmail.com>
2024-02-14 16:20:38 +0530

    first commit

--- app.mjs
+++ app.mjs
@@ -55,6 +55,11 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use((req, res, next) => {
+  console.log('Credentials', req.credentials);
+  next();
+});
+
 // Routes
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
